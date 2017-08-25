81be5893827b4ac2e4e37a8e859d5ecf6fa02ce9
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2017-08-24 21:10:40 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/felipe/marsexplorationrovers/MarsExplorationRoversApplication.java
@@ -0,0 +1,12 @@
+package br.com.felipe.marsexplorationrovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsExplorationRoversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsExplorationRoversApplication.class, args);
+	}
+}
