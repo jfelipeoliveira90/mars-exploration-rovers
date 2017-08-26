0e4d4880a10bdaa8875d9c60ac37334e59f84c2e
Felipe Oliveira <jfelipeoliveira90@gmail.com>
2017-08-26 15:27:39 -0300

    Melhorando impl do left e right commands

--- src/main/java/br/com/felipe/marsexplorationrovers/domain/model/command/impl/RightCommand.java
+++ src/main/java/br/com/felipe/marsexplorationrovers/domain/model/command/impl/RightCommand.java
@@ -1,29 +1,12 @@
 package br.com.felipe.marsexplorationrovers.domain.model.command.impl;
 
-import br.com.felipe.marsexplorationrovers.domain.exceptions.CardinalPointException;
 import br.com.felipe.marsexplorationrovers.domain.model.command.Command;
 import br.com.felipe.marsexplorationrovers.domain.model.probe.Position;
-
-import static br.com.felipe.marsexplorationrovers.domain.model.probe.CardinalPoint.EAST;
-import static br.com.felipe.marsexplorationrovers.domain.model.probe.CardinalPoint.NORTH;
-import static br.com.felipe.marsexplorationrovers.domain.model.probe.CardinalPoint.SOUTH;
-import static br.com.felipe.marsexplorationrovers.domain.model.probe.CardinalPoint.WEST;
 
 public final class RightCommand implements Command {
 
     @Override
     public Position execute(Position position) {
-        switch (position.getCardinalPoint()) {
-            case NORTH:
-                return position.withCardinalPoint(EAST);
-            case EAST:
-                return position.withCardinalPoint(SOUTH);
-            case SOUTH:
-                return position.withCardinalPoint(WEST);
-            case WEST:
-                return position.withCardinalPoint(NORTH);
-            default:
-                throw new CardinalPointException("Ponto cardeal nao implementado");
-        }
+        return position.withCardinalPoint(position.getCardinalPoint().right());
     }
 }
--- src/main/java/br/com/felipe/marsexplorationrovers/domain/model/probe/CardinalPoint.java
+++ src/main/java/br/com/felipe/marsexplorationrovers/domain/model/probe/CardinalPoint.java
@@ -1,16 +1,64 @@
 package br.com.felipe.marsexplorationrovers.domain.model.probe;
 
 public enum CardinalPoint {
-    NORTH("N"),
-    SOUTH("S"),
-    EAST("E"),
-    WEST("W");
+
+    NORTH("N") {
+        @Override
+        public CardinalPoint left() {
+            return WEST;
+        }
+
+        @Override
+        public CardinalPoint right() {
+            return EAST;
+        }
+    },
+
+    SOUTH("S") {
+        @Override
+        public CardinalPoint left() {
+            return EAST;
+        }
+
+        @Override
+        public CardinalPoint right() {
+            return WEST;
+        }
+    },
+
+    EAST("E") {
+        @Override
+        public CardinalPoint left() {
+            return NORTH;
+        }
+
+        @Override
+        public CardinalPoint right() {
+            return SOUTH;
+        }
+    },
+
+    WEST("W") {
+        @Override
+        public CardinalPoint left() {
+            return SOUTH;
+        }
+
+        @Override
+        public CardinalPoint right() {
+            return NORTH;
+        }
+    };
 
     private final String abbreviation;
 
     CardinalPoint(String abbreviation) {
         this.abbreviation = abbreviation;
     }
+
+    public abstract CardinalPoint left();
+
+    public abstract CardinalPoint right();
 
     public String abbreviation() {
         return abbreviation;
